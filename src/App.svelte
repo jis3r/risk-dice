52724dcdef209569052b9578065b2a56f5a726f9
jis3r <jis3r@protonmail.com>
2021-03-19 17:24:54 +0100

    add: basic elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,47 @@
 <script>
-	export let name;
+	let attackers = 0;
+	let defenders = 0;
+	let attackDices = [-1, -1, -1];
+	let defendDices = [-1, -1, -1];
+
+
+	const fight = () => {
+		attackDices = [-1, -1, -1];
+		defendDices = [-1, -1, -1];
+
+		
+		diceThrow();
+
+	}
+
+	function diceThrow() {
+		let dice = Math.ceil( Math.random() *6 );
+		console.log(dice);
+	}
+
+	const reset = () => {
+		attackers = 0;
+		defenders = 0;
+	}
+
+
+
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Risk Dice</h1>
+	<p>Simply input the number of attackers and defenders anmd click fight to see who will win.</p>
+	<label for="attackers">attackers</label>
+	<input type="number" name="attackers" id="attackers" bind:value={attackers}>
+	<label for="attackers">defenders</label>
+	<input type="number" name="defenders" id="attackers" bind:value={defenders}>
+	<br>
+
+	<button on:click={fight}>fight</button>
+	<button on:click={reset}>reset</button>
+
+
 </main>
 
 <style>
