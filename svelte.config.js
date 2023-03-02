8bae25dca34977366127b4fb598fd71d3a5cbae2
jis3r <jis3r@protonmail.com>
2023-03-02 21:02:44 +0100

    config for app-requirements

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	fallback: 'error.html',
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
